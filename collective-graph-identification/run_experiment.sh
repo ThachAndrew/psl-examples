4aade4ab5aea5804915971a6ec4a36387e1954c2
Andrew Thach <andrew.thach123@gmail.com>
2022-11-24 16:09:59 -0800

    Incremented seq iterations.

--- collective-graph-identification/run_experiment.sh
+++ collective-graph-identification/run_experiment.sh
@@ -33,7 +33,7 @@ function main() {
 
     # run_psl "$outDir" "$extraOptions"
 
-    for i in $(seq -w 00 03)
+    for i in $(seq -w 00 04)
     do
       echo "Inferring Fold $i"
       sed -ri "s|([0-9]+)(/\w+)|${i}\2|" cli/collective-graph-identification*.data
